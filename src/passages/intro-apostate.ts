d24f77d58492f2529986b1dd934785718755b80d
Travis Moy <moytravis@gmail.com>
2022-03-07 20:33:57 -0800

    _Done -> _DONE

--- src/passages/intro-apostate.ts
+++ src/passages/intro-apostate.ts
@@ -4,10 +4,10 @@
   _setup.Complete_IntroApostate_MeetingFaithful = function() {
     const _vars = State.variables as any
 
-    if (_vars.IntroFaithfulMeetingApostate_GREETING && _vars.IntroApostate_MeetingFaithful_Done) {
+    if (_vars.IntroFaithfulMeetingApostate_GREETING && _vars.IntroApostate_MeetingFaithful_DONE) {
       const choice = _vars.IntroFaithfulMeetingApostate_GREETING.greeting
       delete _vars.IntroFaithfulMeetingApostate_GREETING
-      delete _vars.IntroApostate_MeetingFaithful_Done
+      delete _vars.IntroApostate_MeetingFaithful_DONE
 
       if (choice === "FAMILIAR") {
         Engine.play("IntroApostateFamiliarGreeting")
