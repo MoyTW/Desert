4fb5eaef74ed64e7589c130a7b84e1f2fdea2593
Travis Moy <moytravis@gmail.com>
2022-03-08 19:37:14 -0800

    words

--- src/passages/intro-apostate.ts
+++ src/passages/intro-apostate.ts
@@ -4,7 +4,8 @@
   _setup.Complete_IntroApostate_MeetingFaithful = function() {
     const _vars = State.variables as any
 
-    if (_vars.IntroFaithfulMeetingApostate_GREETING && _vars.IntroApostate_MeetingFaithful_DONE) {
+    if (_vars.IntroFaithfulMeetingApostate_GREETING &&
+        _vars.IntroApostate_MeetingFaithful_DONE) {
       const choice = _vars.IntroFaithfulMeetingApostate_GREETING.greeting
       delete _vars.IntroFaithfulMeetingApostate_GREETING
       delete _vars.IntroApostate_MeetingFaithful_DONE
@@ -18,4 +19,13 @@
       }
     }
   };
+
+  _setup.Complete_IntroFaithful_MercsEnter = function() {
+    const _vars = State.variables as any
+
+    if (_vars.IntroFaithful_WaitingAwkwardly_CHOICE &&
+        _vars.IntroApostate_MercsEnter_CHOICE) {
+      Engine.play("IntroFaithful_JekuMercsChat")
+    }
+  }
 })()
