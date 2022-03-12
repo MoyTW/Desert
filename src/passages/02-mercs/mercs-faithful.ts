4587afe123628bc7a0bf73a806ad77fe50d589c7
Travis Moy <moytravis@gmail.com>
2022-03-12 12:00:51 -0800

    org faithful

--- src/passages/02-mercs/mercs-faithful.ts
+++ src/passages/02-mercs/mercs-faithful.ts
@@ -6,7 +6,18 @@
 
     if (_vars.MercsFaithful_Start_CHOICE &&
         _vars.MercsApostate_Start_CHOICE) {
-      Engine.play("MercsFaithful_JekuMercsChat")
+      const faithfulChoice = _vars.MercsFaithful_Start_CHOICE.choice
+      const apostateChoice = _vars.MercsApostate_Start_CHOICE.choice
+
+      if (faithfulChoice === "CHAT" && apostateChoice === "TALK") {
+        Engine.play("MercsFaithful_JekuChat_EbiTalk")
+      } else if (faithfulChoice === "CHAT" && apostateChoice === "SILENT") {
+        Engine.play("MercsFaithful_JekuChat_EbiSilent")
+      } else if (faithfulChoice === "REJECT" && apostateChoice === "TALK") {
+        Engine.play("MercsFaithful_JekuReject_EbiTalk")
+      } else {
+        Engine.play("MercsFaithful_JekuReject_EbiSilent")
+      }
     }
   }
 })()
