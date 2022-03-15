1584f6b923a178c6dd89aed7d02d4983731982eb
Travis Moy <moytravis@gmail.com>
2022-03-14 21:29:57 -0700

    clean up apostate

--- src/passages/0X-chase/chase-apostate.ts
+++ src/passages/0X-chase/chase-apostate.ts
@@ -15,7 +15,22 @@
     (State.variables as any).chaseTurn++
   }
 
-  _setup.Complete_ChaseApostate_Christies = function() {
+  const _routingTable = new Map<string, Map<string,string>>([
+    ["ChaseApostate_Christies", new Map<string,string>([
+      ["FOOT", "ChaseApostate_OnFoot"],
+      ["911", "ChaseApostate_911"],
+      ["CAR", "ChaseApostate_Car"],
+      ["KING", "ChaseApostate_King"],
+      ["BAR", "ChaseApostate_Bar"],
+      ["FREEZE", "ChaseApostate_Freeze"],
+    ])],
+    ["ChaseApostate_OnFoot", new Map<string,string>([
+      ["FOOT", "ChaseApostate_OnFoot_2"],
+      ["BACK", "ChaseApostate_Christies"],
+    ])]
+  ])
+
+  _setup.Route_ChaseApostate_Choice = function() {
     const _vars = State.variables as any;
 
     if (!_vars.ChaseApostate_CHOICE || !_vars.ChaseFaithful_CHOICE) { return }
@@ -27,19 +42,7 @@
     delete _vars.ChaseApostate_CHOICE
     delete _vars.ChaseFaithful_CHOICE
 
-    const apostateChoice = _vars.ChaseApostate_CHOICE_LAST.choice
-    if (apostateChoice === "FOOT") {
-      Engine.play("ChaseApostate_OnFoot")
-    } else if (apostateChoice === "911" ) {
-      Engine.play("ChaseApostate_911")
-    } else if (apostateChoice === "CAR" ) {
-      Engine.play("ChaseApostate_Car")
-    } else if (apostateChoice === "KING" ) {
-      Engine.play("ChaseApostate_King")
-    } else if (apostateChoice === "BAR" ) {
-      Engine.play("ChaseApostate_Bar")
-    } else if (apostateChoice === "FREEZE" ) {
-      Engine.play("ChaseApostate_Freeze")
-    }
+    const apostateChoice: string = _vars.ChaseApostate_CHOICE_LAST.choice
+    Engine.play(_routingTable.get(passage())?.get(apostateChoice)!)
   }
 })()
