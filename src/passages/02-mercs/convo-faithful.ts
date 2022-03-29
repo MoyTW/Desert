eb44b5621845ab344370ebf5414f78455a590edc
Travis Moy <moytravis@gmail.com>
2022-03-29 15:14:10 -0700

    convo!!!

--- src/passages/02-mercs/convo-faithful.ts
+++ src/passages/02-mercs/convo-faithful.ts
@@ -22,4 +22,36 @@
       }
     }
   }
+
+  _setup.Complete_ConvoFaithful_Jeku_Implants = function() {
+    const _vars = State.variables as any
+
+    if (_vars.ConvoFaithful_Jeku_Implants && _vars.ConvoApostate_Jeku_Implants) {
+      const apostateChoice = _vars.ConvoApostate_Jeku_Implants.choice
+      delete _vars.ConvoFaithful_Jeku_Implants
+      delete _vars.ConvoApostate_Jeku_Implants
+
+      if (apostateChoice === "TALK") {
+        Engine.play("ConvoFaithful_Jeku_Implants_Talk")
+      } else if (apostateChoice === "EVADE") {
+        Engine.play("ConvoFaithful_Jeku_Implants_Evade")
+      }
+    }
+  }
+
+  _setup.Complete_ConvoFaithful_Ebi_Reno = function() {
+    const _vars = State.variables as any
+
+    if (_vars.ConvoFaithful_Ebi_Reno && _vars.ConvoApostate_Ebi_Reno) {
+      const apostateChoice = _vars.ConvoApostate_Ebi_Reno.choice
+      delete _vars.ConvoFaithful_Ebi_Reno
+      delete _vars.ConvoApostate_Ebi_Reno
+
+      if (apostateChoice === "PUSH") {
+        Engine.play("ConvoFaithful_Ebi_Reno_Push")
+      } else if (apostateChoice === "DROP") {
+        Engine.play("ConvoFaithful_Ebi_Reno_Drop")
+      }
+    }
+  }
 })()
